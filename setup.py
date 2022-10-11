e017362e53a9a759d45f8124b5ae7eefa9d5983d
Al Sweigart <asweigart@gmail.com>
2022-10-11 14:00:39 -0500

    Update pillow install version to 9.2.0

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
         'imapclient==2.1.0',
         #'pyzmail36==1.0.4', # Removed because this can't be installed by setuptools versions after 58.0.0
         'twilio',
-        'pillow==9.0.0',
+        'pillow==9.2.0',
         'pyautogui',
     ],
     keywords="automate boring stuff python",
--- src/automateboringstuff1stedition/__init__.py
+++ src/automateboringstuff1stedition/__init__.py
@@ -1,5 +1,5 @@
 import webbrowser
 
-__version__ = '1.0.4'
+__version__ = '1.0.5'
 
 webbrowser.open('https://inventwithpython.com')
